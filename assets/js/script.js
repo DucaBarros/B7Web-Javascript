602c721b77e5a397e4898db8d79e4380c8043d5a
Eduardo Barros <eduardocarneiro83@gmail.com>
2025-07-07 02:38:25 -0300

    B7Web Javascript até aula Template String

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello World!')
+// Aula Teplate String
+
+let namex = 'Duca'
+let familyName = 'Barros'
+let age = 41
+let fullName = `Meu nome é ${namex} ${familyName} e tenho ${age} anos` //Template String
+
+console.log(fullName)
+
